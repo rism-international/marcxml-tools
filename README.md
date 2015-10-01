e26541650a707e64a6a6b254e358346696735679
Stephan Hirsch <stephan@hirsch-online.com>
2015-10-01 10:27:21 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# RISM Record filter
+
+Helpful links and tutorials
+* RISM Opendata: https://opac.rism.info/index.php?id=8&L=0
+* Regular Expression:  https://en.wikipedia.org/wiki/Regular_expression
+
+## Description
+
+RISM record filter is a small ruby utility program for filtering records from the 
+complete XML open dataset of sources at http://opac.rism.info. 
+
+Filtering is defined by key-value pairs in a YAML-configuration file (default: query.yaml): 
+
+* Key is the Marc21 file (e.g. 100$a)
+* Value is a regular expression (e.g. "Mozart.*")
+
+Query parameters (one per line) are combined with "AND".
+
+## Installation
+
+Requirements
+
+* Ruby
+
---
+++ record_filter.rb
@@ -0,0 +1,70 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'yaml'
+require 'nokogiri'
+require 'trollop'
+
+#OTIONS
+opts = Trollop::options do
+    version "RISM record_filter 1.0"
+    banner <<-EOS
+This utility program searches the complete RISM open data XML-File with parameters of query.yaml
+
+Usage:
+   record_search [options]
+where [options] are:
+EOS
+
+  opt :conf, "Configuration-Filename", :type => :string, :default => "query.yaml"
+  opt :infile, "Input-Filename", :type => :string
+  opt :outfile, "Output-Filename", :type => :string, :default => "out.xml"
+end
+
+Trollop::die :infile, "must exist; you can download it from https://opac.rism.info/fileadmin/user_upload/lod/update/rismAllMARCXML.zip" if !opts[:infile]
+source_file=opts[:infile]
+resfile=opts[:outfile]
+query=YAML.load_file(opts[:conf])
+
+#Helper method to parse huge files with nokogiri
+def each_record(filename, &block)
+  File.open(filename) do |file|
+    Nokogiri::XML::Reader.from_io(file).each do |node|
+      if node.name == 'record' and node.node_type == Nokogiri::XML::Reader::TYPE_ELEMENT
+        yield(Nokogiri::XML(node.outer_xml, nil, "UTF-8"))
+      end
+    end
+  end
+end
+
+cnt=1
+found=0
+ofile=File.open(resfile, 'w')
+
+ofile.write('<?xml version="1.0" encoding="UTF-8"?>'+"\n"+'<collection>'+"\n")
+
+#QUERY
+each_record(source_file) do |record|
+  result={}
+  query.each do |k,v|
+    df=k.split("$")[0]
+    sf=k.split("$")[1]
+    res=record.xpath('//datafield[@tag="'+df+'"]/subfield[@code="'+sf+'"]')
+    res.each do |node|
+      if node.content =~ /#{v}/
+        result[k]=true
+      end
+    end
+  end
+  #RESULT
+  if result.size==query.size
+      found+=1
+      n=Nokogiri::XML(record.to_s, &:noblanks)
+      ofile.puts(n.root.to_xml :indent => 4)
+  end
+  print "\rRecords: #{cnt+=1}"+"\t\t"+"Found: #{found}"
+end
+ofile.puts("</collection>")
+ofile.close
+puts "\n"
+puts "Completed!"
+
