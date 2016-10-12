bf04c76675869591c13b5ec0e10607a6bfa808d0
Stephan Hirsch <stephan@hirsch-online.com>
2016-10-12 16:11:26 +0200

    Copied place to 551

--- lib/muscat/muscat_institution.rb
+++ lib/muscat/muscat_institution.rb
@@ -12,7 +12,7 @@ module Marcxml
     def initialize(node, namespace={'marc' => "http://www.loc.gov/MARC21/slim"})
       @namespace = namespace
       @node = node
-      @methods = [:add_isil, :change_cataloging_source, :repair_leader, :change_type, :map]
+      @methods = [:add_isil, :change_cataloging_source, :repair_leader, :change_type, :copy_110c, :map]
     end
 
     def change_cataloging_source 
@@ -24,6 +24,22 @@ module Marcxml
       subfield=node.xpath("//marc:datafield[@tag='710']/marc:subfield[@code='4']", NAMESPACE)
       subfield.each { |sf| sf.content = convert_type(sf.content) }
     end
+
+    def copy_110c
+      subfield=node.xpath("//marc:datafield[@tag='110']/marc:subfield[@code='c']", NAMESPACE)
+      subfield.each do |sf|
+        tag = Nokogiri::XML::Node.new "datafield", node
+        tag['tag'] = '551'
+        tag['ind1'] = ' '
+        tag['ind2'] = ' '
+        sfa = Nokogiri::XML::Node.new "subfield", node
+        sfa['code'] = 'a'
+        sfa.content = sf.content
+        tag << sfa
+        node.root << tag
+      end
+    end
+
 
     def convert_type(str)
       case str
